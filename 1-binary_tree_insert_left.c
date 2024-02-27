79066c31f7332c627c6c419abc5556f29c3eee0a
ahmedgamaall <ahmedgamalahmed231@gmail.com>
2024-02-28 00:08:59 +0300

    create 1-binary_tree_insert_left.c file

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,10 +15,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *n_nd = malloc(sizeof(binary_tree_t));
 
-	if (parent == NULL){
+	if (parent == NULL)
 		return (NULL);
-		free (n_nd);
-	}
 
 	if (n_nd == NULL)
 		return (NULL);
